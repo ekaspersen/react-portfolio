a15ab16f1316a576d5e47a48f067e9c65ae622f6
Eskil K. Hagen <eskilkaspersen@gmail.com>
2023-06-13 16:59:44 +0200

    added section to my work

--- src/components/MyWork.jsx
+++ src/components/MyWork.jsx
@@ -1,4 +1,8 @@
 import tetradMGif from "../assets/videos/tetrad-marketing.gif";
+import bidhouseShop from "../assets/bidhouse-landing.png";
+import bidhouseBid from "../assets/bidhouse-newbid.png";
+import bidhousePfp from "../assets/bidhouse-pfp.png";
+import bidhouseSignup from "../assets/bidhouse-signup.png";
 import reactlogo from "../assets/icons/react-logo.svg";
 import firebaselogo from "../assets/icons/firebase-logo.svg";
 import tailwindCSSlogo from "../assets/icons/tailwindCSS-logo.svg";
@@ -7,6 +11,7 @@ import vitelogo from "../assets/icons/vite-logo.svg";
 import htmllogo from "../assets/icons/html-logo.svg";
 import csslogo from "../assets/icons/css-logo.svg";
 import jslogo from "../assets/icons/javascript-logo.svg";
+import restapi from "../assets/icons/RestApi.png";
 const MyWork = () => {
     return (
         <div className="flex flex-col">
@@ -29,14 +34,26 @@ const MyWork = () => {
             <section className="min-h-screen flex flex-col-reverse py-16 gap-8 lg:flex-row bg-black">
                 <div className="flex-1 min-h-full flex flex-col gap-8 justify-center items-center">
                     <div className="flex gap-4 flex-wrap px-8 justify-center">
-                        <img src={reactlogo} alt="react logo" />
-                        <img src={tailwindCSSlogo} alt="tailwindCSS logo" />
-                        <img src={htmllogo} alt="HTML shiled" />
-                        <img src={csslogo} alt="CSS shield" />
-                        <img src={jslogo} alt="JS shield" />
-                        <img src={firebaselogo} alt="firebase logo" />
-                        <img src={motionlogo} alt="framer motion logo" />
-                        <img src={vitelogo} alt="Vite Js logo" />
+                        <img src={reactlogo} alt="react logo" title="ReactJs" />
+                        <img
+                            src={tailwindCSSlogo}
+                            alt="tailwindCSS logo"
+                            title="tailwindCSS"
+                        />
+                        <img src={htmllogo} alt="HTML shiled" title="HTML" />
+                        <img src={csslogo} alt="CSS shield" title="CSS" />
+                        <img src={jslogo} alt="JS shield" title="javaScript" />
+                        <img
+                            src={firebaselogo}
+                            alt="firebase logo"
+                            title="Firebase"
+                        />
+                        <img
+                            src={motionlogo}
+                            alt="framer motion logo"
+                            title="Framer Motion"
+                        />
+                        <img src={vitelogo} alt="Vite Js logo" title="ViteJs" />
                     </div>
                     <img
                         className="max-w-full w-11/12 md:w-auto mx-4 border-8 rounded-2xl border-primary-light"
@@ -62,7 +79,7 @@ const MyWork = () => {
                         <a
                             className="block my-2 lg:my-0 py-3 px-8 border-primary border-4 whitespace-nowrap text-primary text-xl font-black"
                             target="_blank"
-                            href="https://tetrad-marketing-react.vercel.app/"
+                            href="https://www.figma.com/file/uTf5RJkqPVaObfhWZplAVn/Tetrad-display-Figma?type=design&node-id=0%3A1&t=nlEYfYVNvekXC6sv-1"
                         >
                             Figma
                         </a>
@@ -90,6 +107,115 @@ const MyWork = () => {
                         Therefore i am putting my portfolio first, soon this
                         site will be done and live on tetradmarketing.no. but
                         for now we have buildt a temporary website with one.com.
+                    </p>
+                </div>
+            </section>
+
+            <section className="min-h-screen flex flex-col-reverse py-16 gap-8 lg:flex-row-reverse bg-primary">
+                <div className="flex-1 min-h-full flex flex-col gap-8 justify-center items-center">
+                    <div className="flex gap-4 flex-wrap px-8 justify-center">
+                        <img src={jslogo} alt="JS shield" title="javaScript" />
+                        <img src={csslogo} alt="CSS shield" title="CSS" />
+                        <img src={htmllogo} alt="HTML shiled" title="HTML" />
+                        <img
+                            src={tailwindCSSlogo}
+                            alt="tailwindCSS logo"
+                            title="TailwindCSS"
+                        />
+                        <img src={vitelogo} alt="Vite Js logo" title="ViteJs" />
+                        <img
+                            className="max-h-9 hover-restapi"
+                            src={restapi}
+                            alt="REST API"
+                            title="REST API"
+                        />
+                    </div>
+                    <div className="gap-4 lg:hidden flex">
+                        <a
+                            className="block my-2 lg:my-0 py-3 bg-secondary-light px-8 whitespace-nowrap text-primary text-xl font-black"
+                            target="_blank"
+                            href="https://github.com/ekaspersen/eskil-kh-semester-project-2"
+                        >
+                            GitHub
+                        </a>
+                        <a
+                            className="block my-2 lg:my-0 py-3 bg-secondary-light px-8 whitespace-nowrap text-primary text-xl font-black"
+                            target="_blank"
+                            href="https://www.figma.com/file/LS7szsiCEJ8hNp8t0hhw3F/SEMESTER-PROJECT-2?type=design&node-id=0%3A1&t=fSFt6kRvshglbse6-1"
+                        >
+                            Figma
+                        </a>
+                    </div>
+                    <div className="flex max-w-lg lg:max-w-full max-h-[50vh] lg:max-h-[60vh] overflow-x-scroll gap-4 lg:gap-8">
+                        <img
+                            className="lg:py-4"
+                            src={bidhouseShop}
+                            alt="Gif of website"
+                        />
+                        <img
+                            className="lg:py-4"
+                            src={bidhousePfp}
+                            alt="Gif of website"
+                        />
+                        <img
+                            className="lg:py-4 "
+                            src={bidhouseBid}
+                            alt="Gif of website"
+                        />
+                        <img
+                            className="lg:py-4 w-fit max-w-xs lg:max-w-full"
+                            src={bidhouseSignup}
+                            alt="Gif of website"
+                        />
+                    </div>
+
+                    <div className="gap-4 hidden lg:flex">
+                        <a
+                            className="block my-2 lg:my-0 py-3 bg-secondary-light px-8 whitespace-nowrap text-primary text-xl font-black"
+                            target="_blank"
+                            href="https://github.com/ekaspersen/eskil-kh-semester-project-2"
+                        >
+                            GitHub
+                        </a>
+                        <a
+                            className="block my-2 lg:my-0 py-3 bg-secondary-light px-8 whitespace-nowrap text-primary text-xl font-black"
+                            target="_blank"
+                            href="https://www.figma.com/file/LS7szsiCEJ8hNp8t0hhw3F/SEMESTER-PROJECT-2?type=design&node-id=0%3A1&t=fSFt6kRvshglbse6-1"
+                        >
+                            Figma
+                        </a>
+                    </div>
+                </div>
+                <div className="flex-1 min-h-full flex flex-col gap-8 justify-center items-center px-8">
+                    <h2 className="text-5xl font-black font-montserrat italic text-secondary-light w-full max-w-long-text">
+                        BidHouse
+                    </h2>
+                    <p className="max-w-long-text">
+                        BidHouse is my semester project exam for my 3rd semester
+                        at Noroff.
+                    </p>
+                    <p className="max-w-long-text">
+                        It is a plain HTML/CSS/JS website using a dummy REST-api
+                        that Noroff had made for us. Using their API a user can
+                        CRUD themselves- They can also read other users bids and
+                        Create, Update and Delete their own bidding rounds.
+                    </p>
+                    <p className="max-w-long-text">
+                        <span className="italic font-montserrat">Brief</span>
+                        <br />
+                        An auction site is looking to launch a website where
+                        users can add items to be bid on and bid on items other
+                        users have put up for auction. When a new user joins the
+                        website, they are given 1000 credits to use on the site.
+                        They can get credits by selling items and use credit by
+                        buying items. Non-registered users can search through
+                        the listings, but only registered users can make bids on
+                        listings. Requi
+                    </p>
+
+                    <p className="max-w-long-text font-black text-sm text-secondary-light50op">
+                        Site is not currently depolyed <br /> Please checkout
+                        Figma(design&planning) and/or github to see my website.
                     </p>
                 </div>
             </section>
